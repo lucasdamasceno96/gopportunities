fcc33e7fdaba96380268462a4674b6a5f2b58d7d
Lucas Damasceno <lucasdamascenopinto96@gmail.com>
2023-05-29 23:40:46 -0300

    logs added

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "gopportunities/router"
+import (
+	"gopportunities/config"
+	"gopportunities/router"
+)
 
+var (
+	logger *config.Logger
+) 
 
 func main() {
+	logger = config.GetLogger("main")
+
+	//initialize config
+	err := config.Init()
+	
+	if err != nil{
+	logger.Error(err)
+	return
+	}
+
+	// initialize router 
 	router.Initialize()
 }
